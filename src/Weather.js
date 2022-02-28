2ea32a9a4a7746201e08c1db312b95cff04703d9
Francesca <93705589+FranSanc@users.noreply.github.com>
2022-02-28 07:25:02 +0000

    Added icon from open weather and adjusted css

--- src/Weather.js
+++ src/Weather.js
@@ -13,7 +13,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
       city: response.data.name,
-      iconUrl: "http://openweathermap.org/img/wn/02d@2x.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
       date: new Date(response.data.dt * 1000),
     });
