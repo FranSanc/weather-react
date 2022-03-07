bf256499dd79b3937fc4426acdc8c506f7935948
Francesca <93705589+FranSanc@users.noreply.github.com>
2022-03-07 11:19:12 +0000

    Added forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -10,17 +10,21 @@ export default function WeatherForecast(props) {
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
-     }
-
-  
+  }
 
   if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]}/>
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
